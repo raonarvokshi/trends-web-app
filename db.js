eda648fef5474a7153400662fa5820c36d2c516a
raonarvokshi <raonar.vokshi@gmail.com>
2025-07-02 14:30:26 +0200

    Fixing db for deployment

--- db.js
+++ db.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DB,
