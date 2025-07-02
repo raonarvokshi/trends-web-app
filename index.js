45d314390a6ede6b79ba1f2f021a65227ac02777
raonarvokshi <raonar.vokshi@gmail.com>
2025-07-02 14:28:33 +0200

    Deployment

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.get("/", verifyToken, (req, res) => {
   res.render("index.ejs", { user: req.user });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on ${localhost}`)
 });
