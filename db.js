bdf034e7e9ce26b58f41e7c388fa64b0b5c8ae0d
raonarvokshi <raonar.vokshi@gmail.com>
2025-07-02 14:41:56 +0200

    Still fixing

--- db.js
+++ db.js
@@ -7,13 +7,8 @@ const db = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PWD,
-  port: process.env.PG_PORT
+  }
 });
-db.connect();
 
+db.connect();
 export default db;
