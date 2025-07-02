541d39bcd62127ba0df9a015ff20c2a6671eb7c5
raonarvokshi <raonar.vokshi@gmail.com>
2025-07-02 15:10:09 +0200

    Still just fixing dhe db for deployment

--- db.js
+++ db.js
@@ -1,14 +1,11 @@
 import pg from "pg";
-import dotenv from "dotenv";
+const { Pool } = pg;
 
-dotenv.config();
-
-const db = new pg.Client({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-db.connect();
 export default db;
